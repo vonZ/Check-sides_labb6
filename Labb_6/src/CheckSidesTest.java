c0cbf074b8a9df2c436f2b3db33941bcb6fb9ad9
vonz <vv.zeipel@gmail.com>
2012-10-15 11:49:15 +0200

    Changed the C - value

--- Labb_6/src/CheckSidesTest.java
+++ Labb_6/src/CheckSidesTest.java
@@ -5,7 +5,7 @@ public class CheckSidesTest extends TestCase {
 
     public void testTriangle() {
         CheckSides tester = new CheckSides();
-        assertEquals("YES", tester.triangle(3, 5, 4));
+        assertEquals("YES", tester.triangle(3, 5, 14));
     }
 
 }
